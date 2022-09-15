40ad29c602869f07a26ed6e761acbaa31850cc0f
Yang20010313 <109203483+Yang20010313@users.noreply.github.com>
2022-09-15 22:28:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,16 +103,16 @@ def get_birthday(birthday, year, today):
  
  
 def get_ciba():
-    url = "http://open.iciba.com/dsapi/"
+    url = "https://api.1314.cool/words/api.php?return=json"
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     r = get(url, headers=headers)
-    note_en = r.json()["content"]
-    note_ch = r.json()["note"]
-    return note_ch, note_en
+    note_en = "这是英语"
+    note_ch = r.json()["word"]
+    return note_ch, note_en 
  
  
 def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
